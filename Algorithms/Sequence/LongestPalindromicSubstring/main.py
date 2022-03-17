61008b1a27205fd39911aa2f42dc3f45c1a5e76f
Hamza Khan <h.akhan@outlook.com>
2022-03-16 23:26:44 -0700

    updated readme

--- Algorithms/Sequence/LongestPalindromicSubstring/main.py
+++ Algorithms/Sequence/LongestPalindromicSubstring/main.py
@@ -1,3 +1,6 @@
+from re import I
+
+
 class LongestPalindromicSubstring:
     """
         Longest Palindromic Substring is a Class that creates instances objects 
@@ -59,6 +62,32 @@ class LongestPalindromicSubstring:
             return max(left, right)
         return recurse(0, len(self.input) + 1)
 
+    def compute_recursion_2(self) -> int:
+        """
+           Recursively enumerate all possible substrings within a given range, check each one whether or not palindrome
+           return right away, else recurse further
+           
+           solution uses a top-down dynamic programming approach where recursion will continue if memo table not already
+           compute max palindromic substring
+
+           time  ~ O(n^2)
+           space ~ O(n^2)
+        """
+        from functools import lru_cache
+        
+        @lru_cache
+        def recurse(i: int, j: int) -> int:
+            # we return rather than recurse as any substring within string
+            # would be smaller, so doesn't matter if another palindrome is nested here
+            if self.is_palindromic(self.input[i:j]):
+                return len(self.input[i:j])
+
+            left = recurse(i + 1, j)
+            right = recurse(i, j - 1)
+
+            return max(left, right)
+        return recurse(0, len(self.input) + 1)
+
     def is_palindromic(self, input: str) -> bool:
         """
             computes whether a given string input is palindromic
@@ -77,6 +106,7 @@ def main():
     l = LongestPalindromicSubstring("abcdefghijklmnracecaropqrstuvwxyz")
     print(l.compute_recursion())
     print(l.compute_iteration())
+    print(l.compute_recursion_2())
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Overview
+
+Various programming problems solved in python
