6b67e0bfbc9efc35d854325e5bfebc822bff5bc2
Hamza Khan <h.akhan@outlook.com>
2022-04-12 00:18:59 -0700

    notes around exceptions

--- LanguageConstructs/ControlFlow/Exceptions/main.py
+++ LanguageConstructs/ControlFlow/Exceptions/main.py
@@ -1,3 +1,12 @@
 #
 # In Python, try/except is commonly used for control flow, and not just for error handling.
 #
+#
+# try
+# except
+# else
+#
+#
+# The built-in exception classes can be subclassed to define new exceptions; 
+# programmers are encouraged to derive new exceptions from the Exception class 
+# or one of its subclasses, and not from BaseException.
