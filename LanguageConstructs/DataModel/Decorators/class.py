0b0156afccda21de4ea1dfe4839e20c0b96c3fdd
Hamza Khan <h.akhan@outlook.com>
2022-04-13 01:10:11 -0700

    added class decorator example

--- LanguageConstructs/DataModel/Decorators/class.py
+++ LanguageConstructs/DataModel/Decorators/class.py
@@ -1,1 +1,31 @@
 # class decorators
+
+
+def singleton(cls):
+    instances = {}
+    def decorator(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    return decorator
+
+@singleton
+class ResourceCheck:
+    def __init__(self, resource: str) -> None:
+        """
+            class's are objects that implement __call__ and thus act 
+            as functions in a sense or objects  
+        """
+        self.resource = resource
+        print('called')
+
+def main():
+    r = ResourceCheck('cpu')
+    rr = ResourceCheck('storage')
+
+    assert r.resource == rr.resource
+
+    print(dir(ResourceCheck))
+
+if __name__ == '__main__':
+    main()
