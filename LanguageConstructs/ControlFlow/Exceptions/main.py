478974eec4ad811f76ef9262314b3f524a7152df
Hamza Khan <h.akhan@outlook.com>
2022-04-12 17:54:35 -0700

    added some files

--- LanguageConstructs/ControlFlow/Exceptions/main.py
+++ LanguageConstructs/ControlFlow/Exceptions/main.py
@@ -10,3 +10,38 @@
 # The built-in exception classes can be subclassed to define new exceptions; 
 # programmers are encouraged to derive new exceptions from the Exception class 
 # or one of its subclasses, and not from BaseException.
+
+class CustomException(Exception):
+    """
+        try / except
+            Catch and recover from exceptions raised by Python, or by you.
+
+        try / finally
+            Perform cleanup actions, whether exceptions occur or not.
+
+        raise
+            Trigger an exception manually in your code.
+        
+        assert
+            Conditionally trigger an exception in your code.
+    """
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+def func():
+    raise CustomException('incorrect usage')
+
+def func2(*args):
+    print(len(args))
+
+def main():
+    try:
+        #func()
+        func2()
+    except CustomException as e:
+        print(e)
+    else:
+        print('Finished executing')
+
+if __name__ == '__main__':
+    main()
