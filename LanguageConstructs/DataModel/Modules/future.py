bf13d75b53ed412226b241b42367323381dced94
Hamza Khan <h.akhan@outlook.com>
2022-04-16 00:09:30 -0700

    added future

---
+++ LanguageConstructs/DataModel/Modules/future.py
@@ -0,0 +1,5 @@
+
+# enables special compilation of code on a per-module basis
+# gradually introduce backs incompatible features
+# https://docs.python.org/3/library/__future__.html
+from __future__ import print_function
