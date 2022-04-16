51b305bb34585ccfb89d2dc13566d17caf40e942
Hamza Khan <h.akhan@outlook.com>
2022-04-16 11:11:11 -0700

    added more notes on hof

--- LanguageConstructs/DataModel/HigherOrderFunctions/main.py
+++ LanguageConstructs/DataModel/HigherOrderFunctions/main.py
@@ -1,3 +1,31 @@
+import functools
+from operator import mul
+
+# The Python Data Model documentation lists seven callable types:
+#   1. User-defined functions
+#       Created with def statements or lambda expressions.
+#
+#   2. Built-in functions
+#       A function implemented in C (for CPython), like len or time.strftime .
+#
+#   3. Built-in methods
+#       Methods implemented in C, like dict.get .
+#
+#   4. Methods
+#       Functions defined in the body of a class.
+#
+#   5. Classes
+#       When invoked, a class runs its __new__ method to create an instance, then __in
+#       it__ to initialize it, and finally the instance is returned to the caller. Because there
+#       is no new operator in Python, calling a class is like calling a function. (Usually calling
+#       a class creates an instance of the same class, but other behaviors are possible by
+#       overriding __new__ .
+#
+#   6. Class instances
+#       If a class defines a __call__ method, then its instances may be invoked as functions.
+#
+#   7. Generator functions
+#       Functions or methods that use the yield keyword. When called, generator functions return a generator object.
 
 def main():
     l = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
@@ -14,5 +42,14 @@ def main():
     llll = [a for a in zip(l, ll)]
     print(llll)
 
+    print(sum([x for x in range(10)]))
+
+    print(functools.reduce(mul, [x+1 for x in range(5)]))
+
+    from functools import partial, reduce
+
+    # partials allows to gen new functions with certains params 'frozen' as an input
+    print([x for x in reversed(list(map(partial(mul, 4), range(1, 5))))])
+    
 if __name__ == '__main__':
     main()
