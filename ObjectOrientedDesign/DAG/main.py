5e2dbf7875f021bcec3287607e27a5901df3a41f
Hamza Khan <h.akhan@outlook.com>
2022-04-21 20:37:44 -0700

    added some initial code around dag design

--- ObjectOrientedDesign/DAG/main.py
+++ ObjectOrientedDesign/DAG/main.py
@@ -1,6 +1,6 @@
-from typing import List  
+from typing import List, Dict
 
-class DAG():
+class DAG:
     """
         Directed Acyclic Graph 
     """
@@ -11,17 +11,56 @@ class DAG():
         """
         def __init__(self, value: int) -> None:
             self.value = value
-            self.edges = List[DAG.Node]
-
+            self.edges = []
+        
+        def insert(self, n):
+            self.edges.append(n)
+        
     def __init__(self, init: List[tuple[int, int]]) -> None:
-        self.init = init
+        self.cache = {}
+        for (src, dst) in init:
+            if src not in self.cache and dst not in self.cache:
+                s = DAG.Node(src)
+                d = DAG.Node(dst)
+                s.insert(d)
+                self.cache[src] = s
+                self.cache[dst] = d
+            elif src not in self.cache:
+                s = DAG.Node(src)
+                d = self.cache[dst]
+                s.insert(d)
+                self.cache[src] = s
+            elif dst not in self.cache:
+                s = self.cache[src]
+                d = DAG.Node(dst)
+                s.insert(d)
+                self.cache[dst] = d
+            else:
+                s = self.cache[src]
+                d = self.cache[dst]
+                s.insert(d)
+
+    def __repr__(self) -> str:
+        buffer = '' 
+        for (key, val) in self.cache.items():
+            buffer += f'{key} --> {sorted([x.value for x in val.edges])}\n'
+        return buffer
 
     def dfs(self, source: int, target: int) -> int:
         return -1
 
 def main():
     print("Constructing DAG")
-    dag = DAG([tuple([1, 3])])
+
+    #
+    #    1 ---> 3 ---> 4
+    #     \     ^ 
+    #      \   /
+    #       v / 
+    #        2
+    #
+    dag = DAG([(1, 3), (1, 2), (2, 3), (3, 4)])
+    print(dag)
 
 if __name__ == '__main__':
     main()
